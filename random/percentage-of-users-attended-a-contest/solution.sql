59274821ce18edebb6803cca1cea78cb380502b6
venexene <venexene@gmail.com>
2026-09-16 14:25:40 +0300

    feat: add percentage-of-users-attended-a-contest

---
+++ random/percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,4 @@
+SELECT contest_id, ROUND(COUNT(user_id)::numeric/(SELECT COUNT(user_id) FROM Users)*100, 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC;
