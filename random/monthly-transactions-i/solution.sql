df36c2aa697cc06ae9d0c731d6688664dad1e0bf
venexene <venexene@gmail.com>
2026-09-16 15:35:22 +0300

    feat: add monthly-transactions-i

---
+++ random/monthly-transactions-i/solution.sql
@@ -0,0 +1,9 @@
+SELECT
+    TO_CHAR(trans_date, 'YYYY-MM') as month,
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(*) FILTER(WHERE state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    COALESCE(SUM(amount) FILTER(WHERE state = 'approved'), 0) AS approved_total_amount
+FROM Transactions
+GROUP BY TO_CHAR(trans_date, 'YYYY-MM') , country
