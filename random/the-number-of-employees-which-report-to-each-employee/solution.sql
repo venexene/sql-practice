e180585a55e3066ce78e5d2a9fdf7cdfad9c3ba0
venexene <venexene@gmail.com>
2026-09-18 12:39:28 +0300

    feat: add primary-department-for-each-employee

---
+++ random/the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -0,0 +1,5 @@
+SELECT e1.employee_id, e1.name, COUNT(*) AS reports_count, AVG(e2.age)::int AS average_age
+FROM Employees e1
+JOIN Employees e2 ON e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id, e1.name
+ORDER BY e1.employee_id;
