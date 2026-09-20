a156b7be933a5ef912a9d1488efb878243f04ad1
venexene <venexene@gmail.com>
2026-09-20 12:55:12 +0300

    feat: add list-the-products-ordered-in-a-period

---
+++ random/list-the-products-ordered-in-a-period/solution.sql
@@ -0,0 +1,7 @@
+SELECT p.product_name, SUM(o.unit) as unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE '2020-02-01'::date <= o.order_date AND o.order_date < '2020-03-01'::date
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100;
